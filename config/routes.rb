1cc06f8182ca6d43d8d278203a1fa7385686ec4f
Bryce Simonds <bsimonds14@gmail.com>
2022-07-06 17:08:38 -0600

    Added route for cars/:id

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '/cars', to: 'cars#index'
+  get '/cars/:id', to: 'cars#show'
   get '/upgrades', to: 'upgrades#index'
 end
