0c186df583c78a9a685b92e1abc84bfa29f8f71c
Bryce Simonds <bsimonds14@gmail.com>
2022-07-06 16:10:03 -0600

    Test: added user story 1 test

--- spec/features/upgrades/index_spec.rb
+++ spec/features/upgrades/index_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
-# User Story 3, Child Index 
+# User Story 1, Parent Index 
 
+# For each parent table
 # As a visitor
-# When I visit '/child_table_name'
-# Then I see each Child in the system including the Child's attributes:
+# When I visit '/parents'
+# Then I see the name of each parent record in the system
 
 RSpec.describe "upgrades index page", type: :feature do 
+    it 'can see the make name of each car record in the system' do 
+        car_1 = Car.create!(make_name: "Toyota",
+                            model_name: "4Runner",
+                            year: 2005,
+                            is_used: true)
+        car_2 = Car.create!(make_name: "Toyota",
+                            model_name: "Camry",
+                            year: 2022,
+                            is_used: false)
+
+        visit '/cars'
+
+        expect(page).to have_content(car_1.make_name)
+        expect(page).to have_content(car_2.make_name)
+    end
+
+
     it "can see all the upgrades car parts names, cost of part, and if needs a mechanic" do 
         upgrade_1 = Upgrade.create!(car_part_name: "Suspension",
                                     cost_of_part: 1200,
