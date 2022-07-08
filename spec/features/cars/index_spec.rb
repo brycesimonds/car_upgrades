d3ae30fefc34df4a95431fd24813905d26294684
Bryce Simonds <bsimonds14@gmail.com>
2022-07-07 18:02:08 -0600

    Test: on car index page shows when each of the records were created

--- spec/features/cars/index_spec.rb
+++ spec/features/cars/index_spec.rb
@@ -60,4 +60,41 @@ RSpec.describe "upgrades index page", type: :feature do
             expect(page).to have_content("Toyota")
         end
     end
+
+    it 'shows when each of the records was created' do 
+        car_1 = Car.create!(brand_of_car: "Toyota",
+            what_line_of_car: "4Runner",
+            year: 2005,
+            is_used: true)
+        car_2 = Car.create!(brand_of_car: "Ford",
+            what_line_of_car: "Taurus",
+            year: 2022,
+            is_used: false)
+        car_3 = Car.create!(brand_of_car: "Kia",
+            what_line_of_car: "Sorento",
+            year: 2013,
+            is_used: true)
+        car_4 = Car.create!(brand_of_car: "Rivian",
+            what_line_of_car: "R1T",
+            year: 2022,
+            is_used: false)
+
+        visit '/cars'
+
+        within "#car-0" do 
+            expect(page).to have_content(car_1.created_at)
+        end
+
+        within "#car-1" do 
+            expect(page).to have_content(car_2.created_at)
+        end
+
+        within "#car-2" do 
+            expect(page).to have_content(car_3.created_at)
+        end
+
+        within "#car-3" do 
+            expect(page).to have_content(car_4.created_at)
+        end
+    end
 end 
