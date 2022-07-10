36abaf0c18b84ab79ab59afc079fd8476606c630
Bryce Simonds <bsimonds14@gmail.com>
2022-07-10 13:45:05 -0600

    controller method: created car_params as a helper method

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -12,10 +12,11 @@ class CarsController < ApplicationController
     end
 
     def create 
-        car = Car.create(brand_of_car: params[:brand_of_car], 
-                         what_line_of_car: params[:what_line_of_car],
-                         year: params[:year],
-                         is_used: params[:is_used])
+        car = Car.create(car_params)
         redirect_to '/cars'
+    end
+
+    def car_params
+        params.permit(:brand_of_car, :what_line_of_car, :year, :is_used)
     end
 end
