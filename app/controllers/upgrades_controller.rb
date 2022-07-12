1079839cc7bb5b16722e5b5e48637285ba42a21e
Bryce Simonds <bsimonds14@gmail.com>
2022-07-12 15:24:38 -0600

    Debug: changed car_id to id

--- app/controllers/upgrades_controller.rb
+++ app/controllers/upgrades_controller.rb
@@ -17,8 +17,15 @@ class UpgradesController < ApplicationController
         redirect_to "/upgrades/#{@upgrade.id}"
     end
 
+    def delete
+        upgrade = Upgrade.find(params[:id])
+        upgrade.destroy
+        redirect_to "/upgrades"
+     end
+
     private 
     def upgrade_params
         params.permit(:car_part_name, :cost_of_part, :need_mechanic)
     end
 end
+
